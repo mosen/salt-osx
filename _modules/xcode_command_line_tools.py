44359d2f1c929c0a5ea89d92abc16c68682ab263
Shea Craig <sheagcraig@gmail.com>
2020-11-12 11:38:46 -0500

    Standardize "" marks and use salt execution modules where possible.

--- _modules/xcode_command_line_tools.py
+++ _modules/xcode_command_line_tools.py
@@ -22,14 +22,14 @@ from salt.exceptions import CommandExecutionError
 
 log = logging.getLogger(__name__)
 
-__virtualname__ = 'xcode_command_line_tools'
+__virtualname__ = "xcode_command_line_tools"
 
 
 def __virtual__():
     """Only load if the platform is macOS"""
-    if __grains__.get('kernel') != 'Darwin':
+    if __grains__.get("kernel") != "Darwin":
         return False
-    if StrictVersion(__grains__['osrelease']) < StrictVersion('10.9'):
+    if StrictVersion(__grains__["osrelease"]) < StrictVersion("10.9"):
         return False
     return __virtualname__
 
@@ -46,11 +46,10 @@ def install():
     # Touch the trigger file to coerce softwareupdate into offering the
     # command line tools.
     trigger = "/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
-    with open(trigger, 'w') as trigger_handle:
-        trigger_handle.write('')
+    __salt__["file.touch"](trigger)
 
-    available_updates = __salt__['softwareupdate.list_available']()
-    logging.debug('Available updates: %s', available_updates)
+    available_updates = __salt__["softwareupdate.list_available"]()
+    logging.debug("Available updates: %s", available_updates)
     # Example label 10.14:
     # Command Line Tools (macOS Mojave version 10.14) for Xcode-10.3
     # Example label Beta:
@@ -58,31 +57,27 @@ def install():
     # Example Catalina result:
     # Command Line Tools for Xcode-11.0
 
-    pattern = re.compile(r'Command Line Tools[\(\)\w\s.]* for Xcode-[\d.]+')
+    pattern = re.compile(r"Command Line Tools[\(\)\w\s.]* for Xcode-[\d.]+")
 
     # Filter out versions that aren't for this OS release.
-    candidates = [(update, version) for update, version in available_updates.items()
-                  if pattern.match(update)]
-    logging.debug('Command Line Tools candidates: %s', candidates)
+    candidates = [
+        (update, version) for update, version in available_updates.items() if pattern.match(update)
+    ]
+    logging.debug("Command Line Tools candidates: %s", candidates)
 
     if candidates:
         # Sort by version number and get the newest (last) update name.
         newest_update = sorted(candidates, key=lambda i: StrictVersion(i[1]))[-1][0]
-        logging.debug('Selected Command Line Tools: %s', newest_update)
-        cmd = ['softwareupdate', '--install', newest_update]
-        __salt__['cmd.run'](cmd)
+        logging.debug("Selected Command Line Tools: %s", newest_update)
+        cmd = ["softwareupdate", "--install", newest_update]
+        __salt__["cmd.run"](cmd)
         result = check()
     else:
         result = False
 
     # Remove the trigger file so softwareupdate doesn't continue to
     # offer the command line tools.
-    try:
-        os.unlink(trigger)
-    except (OSError, FileNotFoundError):
-        # The file got removed already, possibly by the user.
-        # Nothing else to do.
-        pass
+    __salt__["file.remove"](trigger)
 
     return result
 
@@ -98,7 +93,7 @@ def check():
 
         salt '*' xcode_command_line_tools.check
     """
-    cmd = ['xcode-select', '-p']
+    cmd = ["xcode-select", "-p"]
     try:
         return salt.utils.mac_utils.execute_return_success(cmd)
     except CommandExecutionError:
